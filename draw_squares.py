4b94fa959c012e08af25cdb12dc06a97817f1c57
Ben Campbell <ben.in.campbell@gmail.com>
2016-02-12 17:41:41 -0500

    Add draw_squares.py

---
+++ draw_squares.py
@@ -0,0 +1,25 @@
+import turtle
+
+def draw_square(some_turtle):
+    for n in range(0,4):
+        some_turtle.forward(100)
+        some_turtle.right(90)
+
+def draw_multiple_squares():
+    #Set up screen, etc.
+    window = turtle.Screen()
+    window.bgcolor('gray')
+
+    #Brad
+    brad = turtle.Turtle()
+    brad.shape('turtle')
+    brad.color('white', 'gray')
+    brad.speed(20)
+    for i in range(1,181):
+        draw_square(brad)
+        brad.right(2)
+    brad.hideturtle()
+
+    window.exitonclick()
+
+draw_multiple_squares()

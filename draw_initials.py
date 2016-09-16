5488e6361359eb299e47814fd83e8e09187f45fd
Ben Campbell <ben.in.campbell@gmail.com>
2016-09-16 09:53:13 -0400

    Make minor improvements to comments and formatting

--- draw_initials.py
+++ draw_initials.py
@@ -1,35 +1,38 @@
+#!/usr/bin/env python
+
 import turtle
 
+
 def draw_initials():
-    #Set up screen, etc.
+    """Draw the initials BC"""
+    # Set up screen, etc.
     window = turtle.Screen()
     window.bgcolor('gray')
 
-    ###THE LETTER B###
     letter = turtle.Turtle()
     letter.shape('turtle')
     letter.color('white', 'gray')
     letter.speed(8)
-    
-    #Move pen into place
+
+    # Move pen into place
     letter.penup()
     letter.left(180)
     letter.forward(200)
     letter.pendown()
 
-    #Draw the letter B
+    # Draw the letter B
     letter.right(90)
     letter.forward(240)
     letter.right(90)
     letter.forward(65)
-    letter.circle(-55,180)
+    letter.circle(-55, 180)
     letter.forward(65)
     letter.left(180)
     letter.forward(75)
     letter.circle(-65, 180)
     letter.forward(75)
 
-    #Move pen into place
+    # Move pen into place
     letter.penup()
     letter.right(180)
     letter.forward(150)
@@ -41,13 +44,13 @@ def draw_initials():
     letter.forward(80)
     letter.pendown()
 
-    #Draw the letter C
-    letter.circle(-80,-180)
+    # Draw the letter C
+    letter.circle(-80, -180)
     letter.right(180)
     letter.forward(81)
-    letter.circle(80,180)
+    letter.circle(80, 180)
 
-    #Hide turtle and exit drawing window
+    # Hide turtle and exit drawing window
     letter.hideturtle()
     window.exitonclick()
 

52cd9ab1c2e19358dbb2b94cd716b6480fe86157
Ben Campbell <ben.in.campbell@gmail.com>
2016-09-19 11:38:03 -0400

    Make minor improvements to comments and formatting

--- draw_squares.py
+++ draw_squares.py
@@ -1,25 +1,36 @@
+#!/usr/bin/env python
+
 import turtle
 
+
 def draw_square(some_turtle):
-    for n in range(0,4):
+    """Draw one square using Python Turtle"""
+    for n in range(0, 4):
         some_turtle.forward(100)
         some_turtle.right(90)
 
+
 def draw_multiple_squares():
-    #Set up screen, etc.
+    """
+    Draw 1,181 squares using Python Turtle.
+    Each square starts from the same point; the
+    result is a circle made up of squares.
+    """
+    # Set up screen, etc.
     window = turtle.Screen()
     window.bgcolor('black')
 
-    #Brad
+    # Brad
     brad = turtle.Turtle()
     brad.shape('turtle')
     brad.color('white', 'gray')
     brad.speed(20)
-    for i in range(1,181):
+    for i in range(1, 181):
         draw_square(brad)
         brad.right(2)
     brad.hideturtle()
 
     window.exitonclick()
+
 
 draw_multiple_squares()
